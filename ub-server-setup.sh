db08fd2542feb74e70e0c0f22ef850de9a4a0e23
Lucas Robbins <lrobbins013@gmail.com>
2020-05-13 17:33:59 -0700

    added a space to plex wget to fix error; changed wget to save to directory under ~ directory

--- ub-server-setup.sh
+++ ub-server-setup.sh
@@ -11,12 +11,12 @@ apt-get upgrade -y
 
 apt-get install kubuntu-desktop handbrake-cli emacs-nox samba -y
 
-wget -P realvnc/ https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.1-Linux-x64.deb
-dpkg -i realvnc/VNC-Server-6.7.1-Linux-x64.deb
+wget -P ~/realvnc/ https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.1-Linux-x64.deb
+dpkg -i ~/realvnc/VNC-Server-6.7.1-Linux-x64.deb
 systemctl enable vncserver-virtuald.service
 
-wget -P plex/https://downloads.plex.tv/plex-media-server-new/1.19.3.2764-ef515a800/debian/plexmediaserver_1.19.3.2764-ef515a800_amd64.deb
-dpkg -i plex/plexmediaserver_1.19.3.2764-ef515a800_amd64.deb
+wget -P ~/plex/ https://downloads.plex.tv/plex-media-server-new/1.19.3.2764-ef515a800/debian/plexmediaserver_1.19.3.2764-ef515a800_amd64.deb
+dpkg -i ~/plex/plexmediaserver_1.19.3.2764-ef515a800_amd64.deb
 systemctl enable plexmediaserver.service
 
 sudo reboot
