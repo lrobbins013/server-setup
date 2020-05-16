73a0dea45c4b30048b95bac44d12e069ad92d6f2
Lucas Robbins <lrobbins013@gmail.com>
2020-05-15 17:46:21 -0700

    Fixed issue with not having a space between directory and url in last two minecrafter server downloads

--- ub-server-setup.sh
+++ ub-server-setup.sh
@@ -20,8 +20,8 @@ dpkg -i ~/plex/plexmediaserver_1.19.3.2764-ef515a800_amd64.deb
 systemctl enable plexmediaserver.service
 
 wget -P ~/minecraft/1.15.2/ https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
-wget -P ~/minecraft/1.14.4/https://mcversions.net/download/1.14.4
-wget -P ~/minecraft/1.8.9/https://mcversions.net/download/1.8.9
+wget -P ~/minecraft/1.14.4/ https://mcversions.net/download/1.14.4
+wget -P ~/minecraft/1.8.9/ https://mcversions.net/download/1.8.9
 
 #sudo reboot
 echo "Reboot computer for all changes to take effect."
